eb3d3ddd5c18db40b43883188f7c72e5707ba68e
VladADragos <vlad6620@yahoo.com>
2020-02-16 17:16:55 +0100

    added fizzbuzz

---
+++ src/fizzbuzz.rs
@@ -0,0 +1,11 @@
+pub fn fizzbuzz(n: u64) {
+    for i in 1..n {
+        print!("i: {}, ", i);
+        match (i % 3, i % 5) {
+            (0, 0) => println!("fizzbuzz"),
+            (0, _) => println!("fizz"),
+            (_, 0) => println!("buzz"),
+            (_, _) => println!("{}", i),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
 mod fib;
+mod fizzbuzz;
 mod vec;
 
 fn main() {
     println!("Hello, world!");
 
-    let t = String::from("132");
-    t.capacity();
-    let mut point1 = vec::Vec2d::new(4f64, 3f64);
-    let mut point2 = vec::Vec2d::new(3f64, -2f64);
-    let dist = point1.distance(point2);
-
-    println!("{}", dist);
+    fizzbuzz::fizzbuzz(32);
 }
